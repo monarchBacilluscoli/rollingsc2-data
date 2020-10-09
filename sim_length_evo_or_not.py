7e4530381dc602c1da5e5374293f7e3d6538578f
LiuYongfeng <531817981@qq.com>
2020-10-09 21:49:11 +0800

    新的图

--- sim_length_evo_or_not.py
+++ sim_length_evo_or_not.py
@@ -33,6 +33,8 @@ block = ""
 data_name = ""
 whole = pd.DataFrame(columns=raw_colomns)
 
+# 读取数据
+
 while line:  # 读文件
     if (line.find("//") == -1 and line != ""):  # 写入
         block += line
@@ -42,6 +44,8 @@ df = pd.read_csv(StringIO(block), sep="\t", header=None)
 df.columns = raw_colomns
 whole = pd.concat([whole, df])
 
+# 进一步处理
+
 whole.insert(whole.shape[1], nm.is_enemy_pop_evo, 0)
 whole.insert(whole.shape[1], "sim_length", 0)
 whole.insert(whole.shape[1], nm.population_size, 0)
@@ -49,12 +53,25 @@ whole.insert(whole.shape[1], nm.population_size, 0)
 for i in range(0, whole.shape[0]):
     settings_string = whole.loc[i, 'settings']
     whole.loc[i, nm.sim_length] = parse_setting(settings_string, nm.sim_length)
-    whole.loc[i, nm.population_size] = parse_setting(settings_string, nm.population_size)
-    whole.loc[i, nm.is_enemy_pop_evo] = parse_setting(settings_string, nm.is_enemy_pop_evo)
+    whole.loc[i, nm.population_size] = parse_setting(
+        settings_string, nm.population_size)
+    whole.loc[i, nm.is_enemy_pop_evo] = parse_setting(
+        settings_string, nm.is_enemy_pop_evo)
 
-sns.set_theme()
-sns.relplot(data=whole, y="dmg", x=nm.sim_length, hue=nm.is_enemy_pop_evo)
+# 画图
+
+sns.set_theme(style="ticks", palette="pastel")
+
+sns.relplot(data=whole, y="dmg", x=nm.sim_length, hue=nm.is_enemy_pop_evo) # 散点图
+
+plt.figure()
+sns.boxplot(data=whole, y="dmg", x=nm.sim_length, hue=nm.is_enemy_pop_evo) # 箱线图
+
+sns.catplot(data=whole, y="dmg", x=nm.sim_length, hue=nm.is_enemy_pop_evo, kind="point") # 猫图？
 
 plt.show()
 print("end")
+
+
+
 # print(whole.iloc[:, 9])
